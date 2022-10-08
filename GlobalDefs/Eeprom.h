a5d677a387c20f4b77e077b560e1a811d2ebe6d1
Marcin Szkudlinski <szkudlik@gmail.com>
2022-10-08 16:21:40 +0200

    An initial, empty app

---
+++ GlobalDefs/Eeprom.h
@@ -0,0 +1,42 @@
+#ifndef EEPROM_DEFS
+#define EEPROM_DEFS
+
+#include <EEPROM.h>
+
+
+#define EEPROM_CANNARY 0xAA     // change if update changes EEPROM structure
+
+typedef struct __tEeprom
+{
+  uint8_t DeviceID;  // MUST be at the 1st position
+  uint8_t CANNARY;   // a cannary, if is not set to EEPROM_CANNARY the eeprom should be set to default
+#ifdef CONTROLLER
+  uint8_t IP[4];
+  uint8_t IPMask[4];
+  uint8_t Gateway[4];
+  uint8_t DNS[4];
+  uint8_t MAC[6];
+#endif
+};
+
+#define EEPROM_DEVICE_ID_OFFSET offsetof(__tEeprom,DeviceID)
+
+#define EEPROM_CANNARY_OFFSET offsetof(__tEeprom,CANNARY)
+
+#ifdef CONTROLLER
+#define EEPROM_IP offsetof(__tEeprom,IP[0])
+#define EEPROM_IPMASK offsetof(__tEeprom,IPMask[0])
+#define EEPROM_GATEWAY offsetof(__tEeprom,Gateway[0])
+#define EEPROM_DNS offsetof(__tEeprom,DNS[0])
+#define EEPROM_MAC offsetof(__tEeprom,MAC[0])
+#else
+#define EEPROM_IP 0
+#define EEPROM_IPMASK 0
+#define EEPROM_GATEWAY 0
+#define EEPROM_DNS 0
+#define EEPROM_MAC 0
+#endif
+
+void SetDefaultEEPromValues();
+
+#endif //EEPROM_DEFS
---
+++ global.h
@@ -0,0 +1,31 @@
+/*
+ * global.h
+ *
+ * a common file that MUST be included in every .h and .c**
+ *
+ *  Created on: 6 paü 2022
+ *      Author: szkud
+ */
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+// #define CONFIG_TLE8457_serial_lib
+
+#ifdef __AVR_ATmega2560__
+#define CONTROLLER
+#elif __AVR_ATmega328P__
+#else
+#error unknown board
+#endif
+
+
+
+#include <Arduino.h>
+#include "GlobalDefs/version.h"
+#include "GlobalDefs/Eeprom.h"
+#include "src/Common_code/debug.h"
+
+
+
+#endif /* GLOBAL_H_ */
