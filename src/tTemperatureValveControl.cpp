f5ef150218685cc60737ea4dce0d1823df709798
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2022-10-12 16:34:21 +0200

    Impmementation of tTemperatureValveControl

--- src/tTemperatureValveControl.cpp
+++ src/tTemperatureValveControl.cpp
@@ -1,14 +1,109 @@
 /*
  * tTemperatureValveControl.cpp
  *
- *  Created on: 11 paŸ 2022
+ *  Created on: 11 paï¿½ 2022
  *      Author: szkud
  */
 
 #include "../global.h"
 #include "tTemperatureValveControl.h"
+#include "Common_code/sensors/tDS1820Sensor.h"
+
+#define LOCAL_DEBUG
+
 
 void tTemperatureValveControl::onEvent(tSensor *pSensor, tEventType EventType)
 {
+	if (EventType == EV_TYPE_MEASUREMENT_ERROR)
+	{
+		StopValve();
+		return;
+	}
+	if (EventType != EV_TYPE_MEASUREMENT_COMPLETED)
+	{
+		return;
+	}
 
+	tDS1820Sensor::tDS1820Result *pDS1820Result =(tDS1820Sensor::tDS1820Result *) pSensor->getMeasurementBlob();
+	int16_t CurrentTemperature = (pDS1820Result+mSensorDevID)->Temp;
+	int16_t Delta = abs(CurrentTemperature - mTargetTemp);
+	bool doOpen = (mTargetTemp > CurrentTemperature);
+
+	/*
+	 * steps
+	 *  - if state is STATE_FORCE_CLOSE - set valve to closing. STOP
+	 *  - if Delta is bigger than mFastValveMoveThold - set the valve to opening/closing and set proper state (OPENING/CLOSING). STOP.
+	 *  - if delta is smaller than mSlowValveTime:
+	 *  	- if state is CLOSING and Temp is bigger than target ==> Set valve to close slow. STOP
+	 *  	- if state is OPENING and Temp is smaller than target ==> Set valve to open slow. STOP
+	 *  	- if state is IDLE and Delta is bigger than mTolerance - set the valve to opening/closing and set proper state (OPENING/CLOSING). STOP.
+	 *
+	 *  being here means - the temperature is OK,
+	 *  	- stop the valve
+	 *  	- set state to IDLE
+	 */
+
+#ifdef LOCAL_DEBUG
+	   DEBUG_SERIAL.print("-->Temp: ");
+	   DEBUG_SERIAL.print((float)(CurrentTemperature) / 10);
+	   DEBUG_SERIAL.print(" Target: ");
+	   DEBUG_SERIAL.print(getTargetTemp());
+	   DEBUG_SERIAL.print(" Fast: ");
+	   DEBUG_SERIAL.print(getFastThold());
+	   DEBUG_SERIAL.print(" Tolerance: ");
+	   DEBUG_SERIAL.print(getTolerance());
+      DEBUG_SERIAL.print(" Histeresis: ");
+      DEBUG_SERIAL.println(getHisteresis());
+#endif
+
+	if (mState == STATE_FORCE_CLOSE)
+	{
+		CloseValve();
+		return;
+	}
+
+	if (mState == STATE_DISABLED)
+	{
+		StopValve();
+		return;
+	}
+
+	if ( (Delta > mFastValveMoveThold) ||
+		((mState == STATE_IDLE) && (Delta > mHisteresis)) )
+	{
+		// big diffrence in temp or was in idle and run out of tolerance
+		// force valve to open/close
+		if (doOpen)
+		{
+			OpenValve();
+			mState = STATE_OPENING;
+		}
+		else
+		{
+			CloseValve();
+			mState = STATE_CLOSING;
+		}
+
+		return;
+	}
+
+	// delta must be smaller than mFastValveMoveThold
+
+	if (Delta > mTolerance)
+	{
+      if ((mState == STATE_OPENING) && (doOpen))
+      {
+         OpenValveSlow();
+         return;
+      }
+
+      if ((mState == STATE_CLOSING) && (! doOpen))
+      {
+         CloseValveSlow();
+         return;
+      }
+	}
+	// all other cases - switch the valve off
+	StopValve();
+	mState = STATE_IDLE;
 }
--- src/tTemperatureValveControl.h
+++ src/tTemperatureValveControl.h
@@ -1,7 +1,7 @@
 /*
  * tTemperatureValveControl.h
  *
- *  Created on: 11 paŸ 2022
+ *  Created on: 11 paï¿½ 2022
  *      Author: szkud
  */
 
@@ -9,22 +9,72 @@
 #define SRC_TTEMPERATUREVALVECONTROL_H_
 
 #include "../global.h"
-#include "Common_code/sensors/tDS1820Sensor.h"
+#include "tOutputProcessheatingControl.h"
+#include "Common_code/sensors/tSensor.h"
 
+// note that the events are triggered by a sensor thold
+// set sensor to proper time
+// than set the object as a callback to the se
 class tTemperatureValveControl : public tSensorEvent {
 public:
-   tTemperatureValveControl(tDS1820Sensor *pSensor, uint8_t SensorDevID)
-      : mpSensor(pSensor),
-        mSensorDevID(SensorDevID)
-        {
-            pSensor->SetEventCalback(this);
-        }
+	static const uint16_t MAX_VALVE_TIME = 100;
+
+   tTemperatureValveControl(uint8_t SensorDevID, uint8_t ValveOpenOutId, uint8_t ValveCloseOutId, uint16_t SlowValveTime)
+      : mTargetTemp(0),
+        mSensorDevID(SensorDevID),
+		mTolerance(0),
+		mSlowValveTime(SlowValveTime),
+		mFastValveMoveThold(0),
+		mValveOpenOutId(ValveOpenOutId),
+		mValveCloseOutId(ValveCloseOutId),
+		mHisteresis(0),
+		mState(STATE_DISABLED) {}
+
+   void Disable() { mState = STATE_DISABLED; }
+   void Enable()  { mState = STATE_IDLE; }
 
    virtual void onEvent(tSensor *pSensor, tEventType EventType);
 
+   void setTargetTemp(float TargetTemp) { mTargetTemp = TargetTemp * 10; }
+   void setTolerance(float Tolerance)   { mTolerance = Tolerance * 10; }
+   void setFastThold(float FastValveMoveThold) { mFastValveMoveThold = FastValveMoveThold * 10; }
+   void setHisteresis(float Histeresis)   { mHisteresis = Histeresis * 10; }
+
+   float getTargetTemp() const { return ((float)(mTargetTemp) / 10); }
+   float getTolerance()  const { return ((float)(mTolerance) / 10); }
+   float getFastThold()  const { return ((float)(mFastValveMoveThold) / 10); }
+   float getHisteresis() const { return ((float)(mHisteresis) / 10); }
 private:
-   tDS1820Sensor *mpSensor;
+   typedef enum
+   {
+	   STATE_DISABLED,
+	   STATE_IDLE,
+	   STATE_OPENING,
+	   STATE_CLOSING,
+	   STATE_FORCE_CLOSE,
+   } tState;
+
+   tState mState;
+
    uint8_t mSensorDevID;
+   int16_t mTargetTemp;			   //< target temperature (*10)
+   int16_t mFastValveMoveThold;	//< delta thold when the valve is triggered at full speed
+   int16_t mTolerance;           //< delta thold when the temp correction stops
+   int16_t mHisteresis;          //< delta thols when the correction starts
+   uint8_t mValveOpenOutId;
+   uint8_t mValveCloseOutId;
+   uint8_t mSlowValveTime;       //< num of seconds the valve is opened in a single step when moving at slow speed
+
+   void StopValve()  { DEBUG_SERIAL.println("==>STOP");
+      OutputProcess.SetOutput(mValveOpenOutId, 0);  OutputProcess.SetOutput(mValveCloseOutId, 0); }
+   void OpenValve()  { DEBUG_SERIAL.println("==>OPEN");
+      OutputProcess.SetOutput(mValveCloseOutId, 0); OutputProcess.SetOutput(mValveOpenOutId, 1, MAX_VALVE_TIME); }
+   void CloseValve() { DEBUG_SERIAL.println("==>CLOSE");
+      OutputProcess.SetOutput(mValveOpenOutId, 0);  OutputProcess.SetOutput(mValveCloseOutId,1, MAX_VALVE_TIME); }
+   void OpenValveSlow()  { DEBUG_SERIAL.println("==>OPEN SLOW");
+      OutputProcess.SetOutput(mValveCloseOutId, 0); OutputProcess.SetOutput(mValveOpenOutId, 1,mSlowValveTime); }
+   void CloseValveSlow() { DEBUG_SERIAL.println("==>CLOSE SLOW");
+      OutputProcess.SetOutput(mValveOpenOutId, 0);  OutputProcess.SetOutput(mValveCloseOutId, 1,mSlowValveTime); }
 };
 
 #endif /* SRC_TTEMPERATUREVALVECONTROL_H_ */
