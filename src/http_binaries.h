e43790ba228be1f5a17e3d490541435f1a623b86
Marcin Szkudlinski <szkudlik@gmail.com>
2022-10-27 18:40:42 +0200

    Empty JavaScript servlet and content

---
+++ src/http_binaries.h
@@ -0,0 +1,8 @@
+#ifndef HTTP_RAW_BINARIES
+#define HTTP_RAW_BINARIES
+extern "C" {
+#define javascript_js_raw_len 57
+extern const char javascript_js_raw[57];
+
+}
+#endif
--- src/servlets.cpp
+++ src/servlets.cpp
@@ -1,8 +1,8 @@
 #include "servlets.h"
-#include "tOutputProcessheatingControl.h"
+#include "http_binaries.h"
 
 bool tjavaScriptServlet::ProcessAndResponse()
 {
-//	pOwner->SendFlashString(javascript_js_raw,javascript_js_raw_len);
-//	return false;
+	pOwner->SendFlashString(javascript_js_raw,javascript_js_raw_len);
+	return false;
 }
