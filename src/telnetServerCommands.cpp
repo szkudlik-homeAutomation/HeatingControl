39cfdd1aa6cf73dff88d5f8d389116c423cab340
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-12-05 13:17:00 +0100

    Telnet messages cleanup

--- src/telnetServerCommands.cpp
+++ src/telnetServerCommands.cpp
@@ -19,9 +19,9 @@ const commandList_t TelnetCommands[] = {
   {"GetVersion",      send_GetVersion,              "show version"},
   {"Reset",           send_Reset,                   "reset the system"},
   {"ScanActiveNodes", trigger_ScanNodes,            "Scan the bus for nodes from 1 to 32"},
-  {"StateOverview",   send_stateOverviewHandler,    "MESSAGE_TYPE_REQUEST_OVERVIEW_STATE dev_id"},
-  {"OutputState",     send_OutputStateHandler,      "MESSAGE_TYPE_OUTPUT_STATE_REQUEST dev_id out_id"},
-  {"SetOutput",       send_SetOutputHandler,        "MESSAGE_TYPE_OUTPUT_STATE_REQUEST dev_id out_id state [timer] (not set=default, 0-forever)"},
+  {"StateOverview",   send_stateOverviewHandler,    "StateOverview dev_id"},
+  {"OutputState",     send_OutputStateHandler,      "OutputState dev_id out_id"},
+  {"SetOutput",       send_SetOutputHandler,        "SetOutput dev_id out_id state [timer] (not set=default, 0-forever)"},
 #endif //CONFIG_TLE8457_COMM_LIB
 };
 
