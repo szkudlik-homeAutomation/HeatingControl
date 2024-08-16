325d6ec00e36aa952fbdcbc28983b0fe90a6e41a
szkudlik <szkudlik@gmail.com>
2024-08-16 18:10:46 +0200

    Add generalTest application

--- global.h
+++ global.h
@@ -2,10 +2,11 @@
 
 #if APP_HeatingCentral
 #include "generated/HeatingCentral.config.h"
+#elif APP_GeneralTest
+#include "generated/GeneralTest.config.h"
 #else
 #error "Unknown app."
 #endif
-
 #include "src/Common_code/commonGlobals.h"
 
 #include "GlobalDefs/version.h"
