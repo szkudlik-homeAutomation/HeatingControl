c649a39c1b6d4663eeb067dd57bf88302c2a9015
szkudlik <szkudlik@gmail.coim>
2025-06-02 11:02:32 +0200

    Newest common

--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@ set APP_NAME=%1
 
 :: merge main config with app overlay
 
-src\Common_code\scripts\merge_config.py GlobalDefs/common.config appDefs\%APP_NAME%.config generated\%APP_NAME%.config
+python src\Common_code\scripts\merge_config.py GlobalDefs/common.config appDefs\%APP_NAME%.config generated\%APP_NAME%.config
 
 :: generate config
-src\Common_code\scripts\kconfg.py generated\%APP_NAME%.config generated\%APP_NAME%.config.h
+python src\Common_code\scripts\kconfg.py generated\%APP_NAME%.config generated\%APP_NAME%.config.h
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 arduino-cli.exe compile -b arduino:avr:mega:cpu=atmega2560 HeatingControl.ino -v -e --build-property "build.extra_flags=\"-DAPP_%APP_NAME%=1\""
