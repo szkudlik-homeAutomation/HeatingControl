81036a69abefc9b7d321d2e815cadf3cd976878c
szkudlik <szkudlik@gmail.coim>
2025-10-14 20:46:13 +0200

    SHT3 sensor

--- src/Node.cpp
+++ src/Node.cpp
@@ -7,11 +7,8 @@
 #include "Common_code/sensors/tSensorFactory.h"
 #include "Common_code/sensors/tSensor.h"
 
-#include "Common_code/sensors/tDS1820Sensor.h"
-#include "Common_code/sensors/tImpulseSensor.h"
-#include "Common_code/sensors/tPt100AnalogSensor.h"
-#include "Common_code/sensors/tSimpleDigitalInputSensor.h"
 #include "Common_code/sensors/tSystemStatusSensor.h"
+#include "Common_code/sensors/tSht3Sensor.h"
 
 
 class tHomeAutomation: public tApplication {
@@ -29,6 +26,11 @@ protected:
 		// system status, updated every 60s
 		tSensorFactory::Instance->CreateSensor(SENSOR_TYPE_SYSTEM_STATUS, SENSOR_ID_SYSTEM_STATUS,
 					F("SystemStatus"),1,NULL,0,600,true, 1 << EV_TYPE_MEASUREMENT_COMPLETED);
+
+		tSht3Sensor::tConfig tSht3SensorConfig;
+		tSht3SensorConfig.I2C_Addr = SHT30_I2C_ADDR_44;
+		tSht3SensorConfig.Avg = 1;
+		tSensorFactory::Instance->CreateSensor(SENSOR_TYPE_SHT3, 2,F("TempHum"),1,&tSht3SensorConfig,sizeof(tSht3SensorConfig),600,true,  1 << EV_TYPE_MEASUREMENT_COMPLETED);
 	}
 };
 
