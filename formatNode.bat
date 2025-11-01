ce4f2141616b36a4c62bd55d84e079f82b52f9bf
szkudlik <szkudlik@gmail.coim>
2025-11-01 13:29:11 +0100

    lower frequency for USBasp. Comments for sensor bitmap

--- formatNode.bat
+++ formatNode.bat
@@ -11,24 +11,28 @@ set /a "byte2=(%2 >> 16) & 0xFF"
 set /a "byte3=(%2 >> 24) & 0xFF"
 
 ::disable eeprom safe
-..\tools\avrdude.exe -pm328p -cusbasp -Ulfuse:w:0xff:m -Uhfuse:w:0xdf:m -Uefuse:w:0x04:m
+..\tools\avrdude.exe -pm328p -cusbasp -Ulfuse:w:0xff:m -Uhfuse:w:0xdf:m -Uefuse:w:0x04:m -B 125kHz
 
 ::erase chip and upload FW
-..\tools\avrdude.exe -pm328p -cusbasp -e
+..\tools\avrdude.exe -pm328p -cusbasp -e -B 125kHz
 
 ::set chip ID and enabled sensors in flash
-..\tools\avrdude.exe -pm328p -cusbasp -Ueeprom:w:%1,%byte0%,%byte1%,%byte2%,%byte3%:m
+..\tools\avrdude.exe -pm328p -cusbasp -Ueeprom:w:%1,%byte0%,%byte1%,%byte2%,%byte3%:m -B 125kHz
 
 ::enable eeprom safe
-..\tools\avrdude.exe -pm328p -cusbasp -U lfuse:w:0xff:m -U hfuse:w:0xd7:m -U efuse:w:0x04:m
+..\tools\avrdude.exe -pm328p -cusbasp -U lfuse:w:0xff:m -U hfuse:w:0xd7:m -U efuse:w:0x04:m -B 125kHz
 
 ::upload flash
-..\tools\avrdude.exe -pm328p -cusbasp -Uflash:w:build\Node\HeatingControl.ino.hex:i 
+..\tools\avrdude.exe -pm328p -cusbasp -Uflash:w:build\Node\HeatingControl.ino.hex:i -B 125kHz
 
 goto end
 
 :args_count_wrong
 echo usage: format.bat id_of_node(0x for hex) bitmap_of_active_sensors
+echo where sensors are
+echo 01 - system status
+echo 02 - SHT3 temperature and humidity
+echo 04 - TGS_2603 odour sensors
 
 
 :end
